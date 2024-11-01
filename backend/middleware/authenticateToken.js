3fa9616816ee09e8af78de842f25d7ee62f0a14d
ChanwooBrianKim <151323407+ChanwooBrianKim@users.noreply.github.com>
2024-11-01 13:11:01 +1000

    debugging code added

--- backend/middleware/authenticateToken.js
+++ backend/middleware/authenticateToken.js
@@ -1,22 +1,24 @@
-import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
+import jwt from 'jsonwebtoken';
+
 const SECRET_KEY = process.env.SECRET_KEY;
+console.log("SECRET_KEY in authenticateToken:", SECRET_KEY); // Confirm the key
 
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
+    
     if (!token) {
         return res.status(401).json({ message: "Access denied: No token provided" });
     }
 
     jwt.verify(token, SECRET_KEY, (err, user) => {
         if (err) {
+            console.log("Token verification error:", err);
             return res.status(403).json({ message: "Invalid or expired token" });
         }
-        console.log("Authenticated user ID:", user.id); // Log the authenticated user ID
         req.user = user;
         next();
     });
